71795b9031c23077ede0b44548489eadb23fe829
JH <https://github.com/jherman-mcc/MAD-105aboutme.git>
2021-05-27 10:39:27 -0500

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    var myInt: Int = 15
+    var myDoubleInt: Double = 200.00
+    var myByte: Byte = 1
+    var myShort: Short = 90
+    var myLong: Long = 7000000008
+    var myFloat: Float = 500.00f
+    var myBoolean: Boolean = true
+    var myString: String = "This is an easy way to do it."
+
+    println("This value is an integer $myInt")
+    println("This value is a Double Integer $myDoubleInt")
+    println("This value is a Byte $myByte")
+    println("This value is a Short $myShort")
+    println("This value is a Long $myLong")
+    println("This value is a Float $myFloat")
+    println("This value is a Boolean $myBoolean")
+    println("This value is a String $myString")
+}
